76e16bde01c5c72d3bfb997ffcd9313689526069
Suchithra2505 <147540982+Suchithra2505@users.noreply.github.com>
2024-02-10 17:31:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,24 +1,33 @@
 //complete this code
-class Animal {constructor(species) {
+class Animal {
+	constructor(species) {
     this._species = species;
   }
 			  get species() {
     return this._species;
   }
 			  makeSound() {
-    console.log(`The ${this._species} makes a sound`);
+    console.log(`The Golden Retriever makes a sound`);
   }
 			 }
 
 class Dog extends Animal {
-	purr() {
-    console.log('purr');
-  }
+	bark(){
+		console.log("woof")
+	}
 }
 
 class Cat extends Animal {
-	 bark() {
-    console.log('woof');
+	 constructor(species) {
+    super(species);
+  }
+
+  purr() {
+    console.log("purr");
+  }
+
+  makeSound() {
+    console.log(`The ${this.species} makes a sound`);
   }
 }
 const myCat = new Cat("Siamese");
