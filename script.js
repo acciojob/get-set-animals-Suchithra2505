192ce2144e7ebf1db3a426037474db7034cdca89
Suchithra2505 <147540982+Suchithra2505@users.noreply.github.com>
2024-02-10 17:32:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -30,13 +30,13 @@ class Cat extends Animal {
     console.log(`The ${this.species} makes a sound`);
   }
 }
-const myCat = new Cat("Siamese");
-myCat.makeSound(); // Output: The Siamese makes a sound
-myCat.purr(); // Output: purr
+// const myCat = new Cat("Siamese");
+// myCat.makeSound(); // Output: The Siamese makes a sound
+// myCat.purr(); // Output: purr
 
-const myDog = new Dog("Golden Retriever");
-myDog.makeSound(); // Output: The Golden Retriever makes a sound
-myDog.bark(); // Output: woof
+// const myDog = new Dog("Golden Retriever");
+// myDog.makeSound(); // Output: The Golden Retriever makes a sound
+// myDog.bark(); // Output: woof
 // Do not change the code below this line
 window.Animal = Animal;
 window.Dog = Dog;
